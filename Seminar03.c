a0abc6593e342d0b7426ff65b3d8a3dc2674554e
DominicTapangea <dominiqtap37@gmail.com>
2026-03-04 18:21:24 +0200

    Lucrat la seminarul 3

---
+++ Seminar03.c
@@ -0,0 +1,71 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//trebuie sa folositi fisierul masini.txt
+//sau va creati un alt fisier cu alte date
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+
+void afisareMasina(Masina masina) {
+	
+	printf("ID: %d || Numar usi: % d || Pret: %f || Model: %-10s || Nume sofer: %-10s || Serie: %c\n",
+		masina.id, masina.nrUsi, masina.pret, masina.model, masina.numeSofer, masina.serie);
+}
+
+void afisareVectorMasini(Masina* masini, int nrMasini) {
+	
+	if (masini = NULL || nrMasini == 0) {
+		printf("Vectorul de masini este gol\n");
+		return;
+	}
+	for (int i = 0;i < nrMasini;i++)
+		afisareMasina(masini[i]);
+}
+
+void adaugaMasinaInVector(Masina** masini, int * nrMasini, Masina masinaNoua) {
+	//adauga in vectorul primit o noua masina pe care o primim ca parametru
+	//ATENTIE - se modifica numarul de masini din vector;
+	Masina* temp = (Masina*)malloc(sizeof(Masina) * ((*nrMasini) + 1));
+	for (int i = 0;i < *nrMasini;i++) {
+		temp[i] = (*masini)[i];
+	}
+
+	temp[*nrMasini] = masinaNoua;
+	free(*masini);
+
+	*masini = temp;
+	*nrMasini++;
+}
+
+Masina citireMasinaFisier(FILE* file) {
+	//functia citeste o masina dintr-un strceam deja deschis
+	//masina citita este returnata;
+}
+
+Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
+	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+	//prin apelul repetat al functiei citireMasinaFisier()
+	//numarul de masini este determinat prin numarul de citiri din fisier
+	//ATENTIE - la final inchidem fisierul/stream-ul
+}
+
+void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
+	//este dezalocat intreg vectorul de masini
+}
+
+int main() {
+
+
+	return 0;
+}
