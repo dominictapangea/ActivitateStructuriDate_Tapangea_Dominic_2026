2f2dc93216950f7fc7d09fdbab5b3d22e978b86f
DominicTapangea <dominiqtap37@gmail.com>
2026-05-18 22:10:23 +0300

    Structurile de baza

---
+++ Lucru_Individual6.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct StructuraMasina {
+
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+
+typedef struct StructuraMasina Masina;
+
+struct Heap {
+	int lungime;
+	int nrElemViz;
+	Masina* vector;
+};
+
